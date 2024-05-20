194ec4cae2d8f75007e3f71b49eedb951e6bb58e
prayagyadav <prayagyadav206@gmail.com>
2024-05-20 12:40:42 +0530

    Automated Commit

--- pages/components/slider.tsx
+++ pages/components/slider.tsx
@@ -36,7 +36,7 @@ export default function Slider() {
               <img className="w-full h-auto" src={item.url} alt=""  />
 
               {/* title/subtitle */}
-              <h3 className="absolute top-1/2 left-1/2 w-full md:w-auto transform -translate-x-1/2 translate-y-[3rem] md:translate-y-[9rem]  lg:translate-y-48 bg-cyan-600 py-2 lg:py-4 px-2 lg:px-8 text-sm lg:text-sm text-white font-extrabold">
+              <h3 className="absolute top-1/2 left-1/2 w-full md:w-auto transform -translate-x-1/2 translate-y-[3rem] md:translate-y-[9rem]  lg:translate-y-64 py-2 lg:py-4 px-2 lg:px-8 text-sm lg:text-sm backdrop-brightness-50 text-white font-semibold italic">
                 {item.title}
               </h3>
             </div>
