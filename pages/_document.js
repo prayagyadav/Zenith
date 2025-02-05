a56791ef3eab75585ca257a8db2912978eac36a2
prayagyadav <prayagyadav206@gmail.com>
2025-02-05 17:00:40 +0530

    Tryna fix analytics

--- pages/_document.js
+++ pages/_document.js
@@ -13,9 +13,9 @@ export default function Document() {
         />
       </Head>
       <body>
-        <Analytics />
         <Main />
         <NextScript />
+        <Analytics />
       </body>
     </Html>
   );
