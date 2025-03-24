d42a402bea661ba9426c9fd2509e9be97368f496
prayagyadav <prayagyadav206@gmail.com>
2025-03-24 18:31:16 +0530

    Analytics fix

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,3 @@
-import { Analytics } from "@vercel/analytics/react";
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
@@ -15,7 +14,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Analytics />
       </body>
     </Html>
   );
